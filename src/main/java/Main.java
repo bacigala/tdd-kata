0f85c838d3eda33afbe2e5973301309295b689c7
Martin Stolarik <60620588+bacigala@users.noreply.github.com>
2022-03-14 15:21:37 +0100

    done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,35 +8,66 @@ public class Main {
     public static int toInteger(String str) {
         final int FAIL = -9999;
         int result = 0;
-        if (str.contains("-"))
-            return FAIL;
-        if (str.equals("0"))
+        if (str.equals(""))
             return FAIL;
 
 
-        if (str.startsWith("XC"))
-        {
-            str = str.substring(0, str.length() - 2);
-            result = 90;
-        }
-        else if (str.contains("XL"))
-        {
-            str = str.substring(0, str.length() - 2);
-            result = 40;
+        for (int i = 0; i < 3; i++) {
+            if (str.startsWith("M")) {
+                result += 1000;
+                str = str.substring(1);
+            }
         }
 
-        if (str.startsWith("IX"))
-        {
-            str = str.substring(0, str.length() - 2);
+        if (str.startsWith("D")) {
+            str = str.substring(1);
+            result += 500;
+        } else if (str.startsWith("CM")) {
+            str = str.substring(2);
+            result += 900;
+        } else if (str.startsWith("CD")) {
+            str = str.substring(2);
+            result += 400;
+        }
+
+        if (result % 500 == 0) {
+            for (int i = 0; i < 3; i++) {
+                if (str.startsWith("C")) {
+                    result += 100;
+                    str = str.substring(1);
+                }
+            }
+        }
+
+
+        if (str.startsWith("L")) {
+            result += 50;
+            str = str.substring(1);
+        } else if (str.startsWith("XC")) {
+            str = str.substring(2);
+            result += 90;
+        } else if (str.contains("XL")) {
+            str = str.substring(2);
+            result += 40;
+        }
+        if (result % 50 == 0){
+            for (int i = 0; i < 3; i++) {
+                if (str.startsWith("X")) {
+                    result += 10;
+                    str = str.substring(1);
+                }
+            }
+        }
+
+        if (str.startsWith("IX")) {
+            str = str.substring(2);
             result += 9;
             if (str.length() == 0)
                 return result;
             else
                 return FAIL;
-        }
-        else if (str.contains("IV"))
-        {
-            str = str.substring(0, str.length() - 2);
+        } else if (str.contains("IV")) {
+            str = str.substring(2);
             result += 4;
             if (str.length() == 0)
                 return result;
@@ -44,31 +75,22 @@ public class Main {
                 return FAIL;
         }
 
-        if (str.startsWith("L"))
-        {
-            result = 50;
+        if (str.startsWith("V")) {
+            result += 5;
             str = str.substring(1);
         }
 
-        if (str.startsWith("V"))
-        {
-            result = 5;
-            str = str.substring(1);
+        for (int i = 0; i < 3; i++) {
+            if (str.startsWith("I")) {
+                result++;
+                str = str.substring(1);
+            }
         }
 
-        if (str.startsWith("X"))
-        {
-            result = 10;
-            str = str.substring(1);
+        if (str.length() > 0) {
+            return FAIL;
         }
 
-        if (str.startsWith("I"))
-        {
-            if (str.length() > 3)
-                result = FAIL;
-            else
-                result += str.length();
-        }
         return result;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,6 +4,7 @@ class MainTest {
 
     @Test
     void testToInt() {
+        final int FAIL = -9999;
         assert(Main.toInteger("0") == -9999);
         assert(Main.toInteger("-I") == -9999);
         assert(Main.toInteger("I") == 1);
@@ -21,6 +22,45 @@ class MainTest {
         assert(Main.toInteger("XL") == 40);
         assert(Main.toInteger("XC") == 90);
         assert(Main.toInteger("IXIV") == -9999);
+        assert(Main.toInteger("VIV") == -9999);
+        assert(Main.toInteger("CD") == 400);
+        assert(Main.toInteger("CM") == 900);
+        assert(Main.toInteger("CMXC") == 990);
+        assert(Main.toInteger("CMXCIX") == 999);
+        assert(Main.toInteger("CMXCIV") == 994);
+        assert(Main.toInteger("CDXCIX") == 499);
+        assert(Main.toInteger("CDXC") == 490);
+        assert(Main.toInteger("LIV") == 54);
+        assert(Main.toInteger("CDL") == 450);
+        assert(Main.toInteger("DXL") == 540);
+        assert(Main.toInteger("IXX") == -9999);
+        assert(Main.toInteger("XXX") == 30);
+        assert(Main.toInteger("XXXX") == -9999);
+        assert(Main.toInteger("M") == 1000);
+        assert(Main.toInteger("MM") == 2000);
+        assert(Main.toInteger("MMMM") == -9999);
+        assert(Main.toInteger("MMMCMXCIX") == 3999);
+        assert(Main.toInteger("MMMDCCCXL") == 3840);
+        assert(Main.toInteger("CDCC") == FAIL);
+        assert(Main.toInteger("CC") == 200);
+        assert(Main.toInteger("DCII") == 602);
+        assert(Main.toInteger("CDII") == 402);
+        assert(Main.toInteger("VV") == FAIL);
+        assert(Main.toInteger("LL") == FAIL);
+        assert(Main.toInteger("DD") == FAIL);
+        assert(Main.toInteger("LXXXIX") == 89);
+        assert(Main.toInteger("DCCCLXXXIX") == 889);
+        assert(Main.toInteger("XCIX") == 99);
+        assert(Main.toInteger("x") == FAIL);
+        assert(Main.toInteger("") == FAIL);
+        assert(Main.toInteger("XCIV") == 94);
+        assert(Main.toInteger("DCCLXXXIX") == 789);
+        assert(Main.toInteger("MMCDXXI") == 2421);
+        assert(Main.toInteger("MIX") == 1009);
+        assert(Main.toInteger("MMMM") == FAIL);
+        assert(Main.toInteger("XCX") == FAIL);
+        assert(Main.toInteger("IXI") == FAIL);
+        assert(Main.toInteger("XV") == 15);
     }
 
 }
