239f6a9fe7ba5d188322d08a5de8c0735494843e
Martin <martin.vanko96@gmail.com>
2022-03-27 18:33:43 +0200

    del comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,37 +4,8 @@ import java.util.*;
 
 public class Main {
 
-
-    private static Scanner scanner;
-    private static final String delim = ";";
-    private static int colsCount = 0;
-
-
     public static void main(String[] args) throws FileNotFoundException {
-//        scanner = OpenFile("./suborDat.csv");
-//
-//        int actualCols;
-//        int lineCounter = 1;
-//
-//        String line = GetNextLine(scanner);
-//        tokens = ParseLine(line, delim);
-//        int expectedColsCount = tokens.size();
-//
-//        while (line != null) {
-//            line = GetNextLine(scanner);
-//            tokens = ParseLine(line, delim);
-//            actualCols = CheckCols(expectedColsCount, tokens);
-//            if (actualCols != 0)
-//                System.out.println("Columns at line " + lineCounter + " are not equal header with " + colsCount +
-//                        " columns, has only " + actualCols + " column/s.");
-//            lineCounter++;
-//
-//        }
     }
-
-//    public static int CheckCols(int ExpectedColsCount, Scanner scan) {
-//        String line = GetNextLine(scanner);
-//    }
 
     public static List<String> ParseLine(String line, String delim) {
         List<String> tokens = Arrays.asList(line.split(delim));
