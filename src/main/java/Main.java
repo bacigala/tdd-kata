b223d8fa2d82f8d919c3feb86b789212e51774bd
Martin <martin.vanko96@gmail.com>
2022-03-27 14:31:22 +0200

    open file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 
 public class Main {
 
@@ -5,4 +8,13 @@ public class Main {
 
     }
 
+    public static Scanner OpenFile(String filename) throws FileNotFoundException {
+        try {
+            File myObj = new File(filename);
+            return new Scanner(myObj);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            throw e;
+        }
+    }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,10 +1,22 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MainTest {
 
     @Test
-    void test() {
-
+    void OpenFileTest() {
+        Assertions.assertThrows(FileNotFoundException.class, () -> {
+            Main.OpenFile("");
+        });
+        Assertions.assertDoesNotThrow(() -> {
+            Scanner obj = Main.OpenFile("./suborDat.csv");
+            Assertions.assertTrue(obj instanceof Scanner);
+        });
     }
 
 }
