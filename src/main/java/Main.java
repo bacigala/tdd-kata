a069d5b3754aee009d6acea2cd3fd633293a973b
Martin <martin.vanko96@gmail.com>
2022-03-27 18:32:37 +0200

    unit tests for data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,43 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner; // Import the Scanner class to read text files
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
 
+    private static Scanner scanner;
+    private static final String delim = ";";
+    private static int colsCount = 0;
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+//        scanner = OpenFile("./suborDat.csv");
+//
+//        int actualCols;
+//        int lineCounter = 1;
+//
+//        String line = GetNextLine(scanner);
+//        tokens = ParseLine(line, delim);
+//        int expectedColsCount = tokens.size();
+//
+//        while (line != null) {
+//            line = GetNextLine(scanner);
+//            tokens = ParseLine(line, delim);
+//            actualCols = CheckCols(expectedColsCount, tokens);
+//            if (actualCols != 0)
+//                System.out.println("Columns at line " + lineCounter + " are not equal header with " + colsCount +
+//                        " columns, has only " + actualCols + " column/s.");
+//            lineCounter++;
+//
+//        }
     }
 
+//    public static int CheckCols(int ExpectedColsCount, Scanner scan) {
+//        String line = GetNextLine(scanner);
+//    }
+
     public static List<String> ParseLine(String line, String delim) {
-        List<String> tokens = new ArrayList<>();
-        StringTokenizer tokenizer = new StringTokenizer(line, delim);
-        while (tokenizer.hasMoreElements())
-            tokens.add(tokenizer.nextToken());
+        List<String> tokens = Arrays.asList(line.split(delim));
         return tokens;
     }
 
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,14 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.jupiter.api.*;
 
 import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Scanner;
+import org.assertj.core.api.SoftAssertions;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 
 class MainTest {
 
@@ -28,25 +33,133 @@ class MainTest {
         String line = Main.GetNextLine(obj);
         Assertions.assertNotNull(line);
         Assertions.assertTrue(line instanceof String);
+        Assertions.assertTrue(line.startsWith("cisdot"));
+        line = Main.GetNextLine(obj);
+        Assertions.assertTrue(!(line.startsWith("cisdot")));
     }
 
     @Test
     void TestParseLine() throws FileNotFoundException {
+        String line = "1;;2";
+        Assertions.assertEquals(3, Main.ParseLine(line, delim).size());
+
         Scanner obj = Main.OpenFile("./suborDat.csv");
         Assertions.assertTrue(obj instanceof Scanner);
-        String line = Main.GetNextLine(obj);
+        line = Main.GetNextLine(obj);
         Assertions.assertNotNull(line);
         Assertions.assertTrue(line instanceof String);
 
         List<String> tokens = Main.ParseLine(line, delim);
-        Assertions.assertEquals(tokens.size(), colsCount);
+        Assertions.assertEquals(colsCount, tokens.size());
     }
 }
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TestData {
 
-    @Test
-    void TestFileCols() {
+    private String filename = "./suborDat.csv";
+    private final String delim = ";";
+    private int ExpectedColsCountFromHeader;
+    private List<String> cols;
+    private int fileLineNumber;
+    private int lineCounter;
+    private SoftAssertions softAssertions;
+    private Scanner scanner;
 
+
+    @BeforeEach
+    void SetUp() throws FileNotFoundException {
+        scanner = Main.OpenFile(filename);
+        Assertions.assertNotEquals(scanner, null);
+        lineCounter = 1;
+        softAssertions = new SoftAssertions();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        softAssertions.assertAll();
+        scanner.close();
+//        if (!cols.isEmpty())
+//            cols.clear();
+    }
+
+    @Test
+    void TestColumnsCount() {
+        String line = Main.GetNextLine(scanner);
+        Assertions.assertNotNull(line);
+        ExpectedColsCountFromHeader = Main.ParseLine(line, delim).size();
+        line = Main.GetNextLine(scanner);
+        while (line != null) {
+            cols = Main.ParseLine(line, delim);
+            softAssertions.assertThat(cols.size()).isEqualTo(ExpectedColsCountFromHeader);
+            line = Main.GetNextLine(scanner);
+        }
+    }
+
+    @Test
+    void TestLineNumbers() {
+        String line = Main.GetNextLine(scanner);
+        Main.ParseLine(line, delim).size();
+        line = Main.GetNextLine(scanner);
+        while (line != null) {
+            cols = Main.ParseLine(line, delim);
+            fileLineNumber = Integer.parseInt(cols.get(0));
+            softAssertions.assertThat(lineCounter).isEqualTo(fileLineNumber);
+            if (lineCounter != fileLineNumber)
+                lineCounter = fileLineNumber;
+            line = Main.GetNextLine(scanner);
+            lineCounter++;
+        }
+    }
+
+    @Test
+    void TestQuestionsScale1to7() {
+        String line = Main.GetNextLine(scanner);
+        Main.ParseLine(line, delim).size();
+        line = Main.GetNextLine(scanner);
+        String[] allowed = { "1", "2", "3", "4", "5", "6", "7", "9", "0" };
+        int[] questions = {1, 2, 3, 4, 5, 6, 7, 8, 9, 21, 28, 29, 30, 31, 32, 33, 34, 35, 36};
+        while (line != null) {
+            cols = Main.ParseLine(line, delim);
+            for (int i = 0; i < questions.length; i++) {
+                if (cols.size() > questions[i])
+                    softAssertions.assertThat(cols.get(questions[i])).containsAnyOf(allowed);
+            }
+            line = Main.GetNextLine(scanner);
+        }
+    }
+
+    @Test
+    void TestQuestionsScale1to4() {
+        String line = Main.GetNextLine(scanner);
+        Main.ParseLine(line, delim).size();
+        line = Main.GetNextLine(scanner);
+        String[] allowed = {"1", "2", "3", "4", "9", "0" };
+        int[] questions = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 23, 24, 25, 26, 27};
+        while (line != null) {
+            cols = Main.ParseLine(line, delim);
+            for (int i = 0; i < questions.length; i++) {
+                if (cols.size() > questions[i])
+                    softAssertions.assertThat(cols.get(questions[i])).containsAnyOf(allowed);
+            }
+            line = Main.GetNextLine(scanner);
+        }
+    }
+
+    @Test
+    void TestQuestionsScale1to5() {
+        String line = Main.GetNextLine(scanner);
+        Main.ParseLine(line, delim).size();
+        line = Main.GetNextLine(scanner);
+        String[] allowed = {"1", "2", "3", "4", "5", "9", "0" };
+        int[] questions = {22};
+        while (line != null) {
+            cols = Main.ParseLine(line, delim);
+            for (int i = 0; i < questions.length; i++) {
+                if (cols.size() > questions[i])
+                    softAssertions.assertThat(cols.get(questions[i])).containsAnyOf(allowed);
+            }
+            line = Main.GetNextLine(scanner);
+        }
     }
 }
