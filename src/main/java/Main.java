186af056d1c4d330b474b2a4f6bb1ee129ad0ef2
Martin <martin.vanko96@gmail.com>
2022-04-04 19:27:12 +0200

    thousands loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,92 +2,107 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(toInteger("I"));
+        Main m = new Main();
+        System.out.println(m.RomanToDecimal("I"));
     }
+    private Roman thousand = new Roman("M", 1000);
+    private String romanInput;
 
-    public static int toInteger(String str) {
-        final int FAIL = -9999;
+    public int Blah(String roman, int decimal) {
         int result = 0;
-        if (str == null || str.equals(""))
-            return FAIL;
-
-
         for (int i = 0; i < 3; i++) {
-            if (str.startsWith("M")) {
-                result += 1000;
-                str = str.substring(1);
+            if (romanInput.startsWith(roman)) {
+                result += decimal;
+                romanInput = romanInput.substring(1);
             }
         }
+        return result;
+    }
 
-        if (str.startsWith("D")) {
-            str = str.substring(1);
+    public int RomanToDecimal(String str) {
+        romanInput = str;
+        final int FAIL = -9999;
+        int result = 0;
+        if (romanInput == null || romanInput.equals(""))
+            return FAIL;
+
+        result += Blah(thousand.roman, thousand.decimal);
+//        for (int i = 0; i < 3; i++) {
+//            if (romanInput.startsWith("M")) {
+//                result += 1000;
+//                romanInput = romanInput.substring(1);
+//            }
+//        }
+
+        if (romanInput.startsWith("D")) {
+            romanInput = romanInput.substring(1);
             result += 500;
-        } else if (str.startsWith("CM")) {
-            str = str.substring(2);
+        } else if (romanInput.startsWith("CM")) {
+            romanInput = romanInput.substring(2);
             result += 900;
-        } else if (str.startsWith("CD")) {
-            str = str.substring(2);
+        } else if (romanInput.startsWith("CD")) {
+            romanInput = romanInput.substring(2);
             result += 400;
         }
 
         if (result % 500 == 0) {
             for (int i = 0; i < 3; i++) {
-                if (str.startsWith("C")) {
+                if (romanInput.startsWith("C")) {
                     result += 100;
-                    str = str.substring(1);
+                    romanInput = romanInput.substring(1);
                 }
             }
         }
 
 
-        if (str.startsWith("L")) {
+        if (romanInput.startsWith("L")) {
             result += 50;
-            str = str.substring(1);
-        } else if (str.startsWith("XC")) {
-            str = str.substring(2);
+            romanInput = romanInput.substring(1);
+        } else if (romanInput.startsWith("XC")) {
+            romanInput = romanInput.substring(2);
             result += 90;
-        } else if (str.contains("XL")) {
-            str = str.substring(2);
+        } else if (romanInput.contains("XL")) {
+            romanInput = romanInput.substring(2);
             result += 40;
         }
         if (result % 50 == 0){
             for (int i = 0; i < 3; i++) {
-                if (str.startsWith("X")) {
+                if (romanInput.startsWith("X")) {
                     result += 10;
-                    str = str.substring(1);
+                    romanInput = romanInput.substring(1);
                 }
             }
         }
 
-        if (str.startsWith("IX")) {
-            str = str.substring(2);
+        if (romanInput.startsWith("IX")) {
+            romanInput = romanInput.substring(2);
             result += 9;
-            if (str.length() == 0)
+            if (romanInput.length() == 0)
                 return result;
             else
                 return FAIL;
-        } else if (str.contains("IV")) {
-            str = str.substring(2);
+        } else if (romanInput.contains("IV")) {
+            romanInput = romanInput.substring(2);
             result += 4;
-            if (str.length() == 0)
+            if (romanInput.length() == 0)
                 return result;
             else
                 return FAIL;
         }
 
-        if (str.startsWith("V")) {
+        if (romanInput.startsWith("V")) {
             result += 5;
-            str = str.substring(1);
+            romanInput = romanInput.substring(1);
         }
 
         for (int i = 0; i < 3; i++) {
-            if (str.startsWith("I")) {
+            if (romanInput.startsWith("I")) {
                 result++;
-                str = str.substring(1);
+                romanInput = romanInput.substring(1);
             }
         }
 
-        if (str.length() > 0) {
+        if (romanInput.length() > 0) {
             return FAIL;
         }
 
---
+++ src/main/java/Roman.java
@@ -0,0 +1,9 @@
+public class Roman {
+    public int decimal;
+    public String roman;
+    public Roman(String roman, int decimal)
+    {
+        this.roman = roman;
+        this.decimal = decimal;
+    }
+}
